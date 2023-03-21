63fd983f7853aa7b822ce1ed2cf0743f3d0eabcb
Shiwangi Keshri <shiwangi.keshri@ericsson.com>
2023-03-21 23:16:37 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent=React.createElement("div",{},[
+    React.createElement("h1",{},"I am h1"),React.createElement("h2",{},"hi bro")
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
